1030ed81f92ca43a6b152fbeb7aa60a2bf8522c3
LasseNatvig <lasse.natvig@ntnu.no>
2022-06-30 14:41:43 +0200

    update

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if settings.USER_INTERFACE_MODE == "CMD" or not GUI_main():
     ###############################################################################
     # get initial state
 
-    #cityURL = "https://data.urbansharing.com/oslobysykkel.no/trips/v1/"
+    # cityURL = "https://data.urbansharing.com/oslobysykkel.no/trips/v1/"
     cityURL = "https://data.urbansharing.com/oslovintersykkel.no/trips/v1/"
     #cityURL = "https://data.urbansharing.com/bergenbysykkel.no/trips/v1/"
     #cityURL = "https://data.urbansharing.com/trondheimbysykkel.no/trips/v1/"
