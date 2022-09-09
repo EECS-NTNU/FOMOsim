a6ad7cb0986e23e7439fdab98439909fcd78775a
LasseNatvig <lasse.natvig@ntnu.no>
2022-09-09 21:40:39 +0200

    new plot, no complot

--- exp_lasse2.py
+++ exp_lasse2.py
@@ -9,7 +9,7 @@ import policies.fosen_haldorsen
 import policies.haflan_haga_spetalen
 import target_state
 import matplotlib.pyplot as plt
-from matplotlib import cm 
+from matplotlib import cm, colors
 import numpy as np
 from helpers import *
 
@@ -19,15 +19,10 @@ def Surface3Dplot(bikes, policyNames, values, title):
     bikes, policiesPosition = np.meshgrid(bikes, policiesPosition)
     values2D = bikes + policiesPosition*100 # 2D numpyarray
     Z = values2D # Z behaves as a pointer/reference to values2D
-
-    # copy results from 2D list to numpyarray
-    for p in range(len(policyNames)):
+    for p in range(len(policyNames)):  # copy results from 2D list to numpyarray
         for b in range(len(bikes[p])):
-            # print("p:b", p, ":", b)
             Z[p][b] = values[p][b] 
-
-    surf = ax.plot_surface(bikes, policiesPosition, Z, cmap=cm.coolwarm,
-                        linewidth=0, antialiased=False)
+    surf = ax.plot_surface(bikes, policiesPosition, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
     ax.set_xlabel("number of bikes")
     ax.set_ylabel("rebalancing policy")
     ax.set_yticks(range(len(policyNames)))
@@ -40,18 +35,54 @@ def Surface3Dplot(bikes, policyNames, values, title):
     fig.suptitle(title)
     return fig, ax
 
+def Surface3DplotFraction(bikes, policyNames, starv, cong, title):
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    policiesPosition = range(len(policyNames))
+    bikes, policiesPosition = np.meshgrid(bikes, policiesPosition)
+    Z = bikes + policiesPosition # 2D numpyarray, addition is not used, but gives Z the right shape, Z behaves now as a reference to a 2D-matrix
+    Rint = np.copy(Z)
+    R = Rint.astype(float) 
+    for p in range(len(policyNames)): # copy results from 2D list to numpyarray
+        for b in range(len(bikes[p])):
+            Z[p][b] = starv[p][b] + cong[p][b] # Z-value to plot, with zero at zero, in range 0 - 120 or more
+    for p in range(len(policyNames)): # copy results from 2D list to numpyarray
+        for b in range(len(bikes[p])):
+            R[p][b] = (starv[p][b]/Z[p][b])*100 # Starvation in % of Lost trips  
+    norm = colors.Normalize(vmin=0, vmax=120)
+    norm2 = colors.Normalize(vmin=0.0, vmax=100)
+    scmap = plt.cm.ScalarMappable(norm = norm, cmap="coolwarm")
+    scmap2 = plt.cm.ScalarMappable(norm = norm2, cmap=cm.BrBG)
+    surf = ax.plot_surface(bikes, policiesPosition, Z*2, facecolors=scmap.to_rgba(Z), shade=False)
+    surf2 = ax.plot_surface(bikes, policiesPosition, (R*0)-100, facecolors=scmap2.to_rgba(R), shade=False)
+    ax.set_zlim(-100, 120) 
+    ax.set_xlabel("number of bikes", weight='bold')
+    ax.set_ylabel("rebalancing policy",  weight='bold')
+    ax.set_yticks(range(len(policyNames)))
+    ax.set_yticklabels(policyNames)
+    # https://stackoverflow.com/questions/6390393/matplotlib-make-tick-labels-font-size-smaller
+    ax.tick_params(axis='both', which='major', labelsize=7)
+    ax.tick_params(axis='both', which='minor', labelsize=8)
+    # yaxis-set_(policies, policyNames)
+    cb = fig.colorbar(scmap, shrink=0.2, aspect=5, location = "right")
+    cb2 = fig.colorbar(scmap2, shrink=0.2, aspect=5, location = "bottom")
+    cb.set_label(label="Lost trips in " + '%' + " of total trips", size = 9)
+    cb2.set_label(label = "Congestion - starvation ratio", size = 9)
+    plt.tight_layout()
+    fig.suptitle(title)
+    return fig, ax
+
+
 DURATION = timeInMinutes(hours=48)
-instances = [ ("Oslo", "https://data.urbansharing.com/oslobysykkel.no/trips/v1/", None,  None,   33,   0,    DURATION ),
-]
+instances = [ ("Oslo", "https://data.urbansharing.com/oslobysykkel.no/trips/v1/", None,  None,   33,   0,    DURATION )]
 analyses = [
     # Name,        target_state,                                 policy,                  numvehicles
-    ("equalprob-2",  target_state.equal_prob_target_state,         policies.GreedyPolicy(),           2),
-    ("equalprob-1",  target_state.equal_prob_target_state,         policies.GreedyPolicy(),           1),
-    ("outflow-2",    target_state.outflow_target_state,            policies.GreedyPolicy(),           2),
-    ("outflow-1",    target_state.outflow_target_state,            policies.GreedyPolicy(),           1),
-    ("evenly-2",     target_state.evenly_distributed_target_state, policies.GreedyPolicy(),           2),
-    ("evenly-1",     target_state.evenly_distributed_target_state, policies.GreedyPolicy(),           1),
-    ("random-2",     target_state.evenly_distributed_target_state, policies.RandomActionPolicy(),     2),
+    # ("equalprob-2",  target_state.equal_prob_target_state,         policies.GreedyPolicy(),           2),
+    # ("equalprob-1",  target_state.equal_prob_target_state,         policies.GreedyPolicy(),           1),
+    # ("outflow-2",    target_state.outflow_target_state,            policies.GreedyPolicy(),           2),
+    # ("outflow-1",    target_state.outflow_target_state,            policies.GreedyPolicy(),           1),
+    # ("evenly-2",     target_state.evenly_distributed_target_state, policies.GreedyPolicy(),           2),
+    # ("evenly-1",     target_state.evenly_distributed_target_state, policies.GreedyPolicy(),           1),
+    # ("random-2",     target_state.evenly_distributed_target_state, policies.RandomActionPolicy(),     2),
     ("random-1",     target_state.evenly_distributed_target_state, policies.RandomActionPolicy(),     1),
     ("do_nothing",   target_state.evenly_distributed_target_state, policies.DoNothing(),              1),
 ]
@@ -62,7 +93,7 @@ for ana in analyses:
 policyIndices = range(len(policyNames))
 
 seeds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-seeds = [0,1,2]
+seeds = [0,1]
 
 def lostTripsPlot(cities, policies, starv, starv_stdev, cong, cong_stdev):
     fig, subPlots = plt.subplots(nrows=1, ncols=len(cities), sharey=True)
@@ -155,10 +186,10 @@ if __name__ == "__main__":
     print(resultsCongestion)
     print(resultsTotal)
 
-
     fig, ax = Surface3Dplot(bikes, policyNames, resultsStarvation, "Starvation (" + '%' + " of trips)")
     fig, ax = Surface3Dplot(bikes, policyNames, resultsCongestion, "Congestion (" + '%' + " of trips)")
     fig, ax = Surface3Dplot(bikes, policyNames, resultsTotal, "Starvation + congestion (" + '%' + " of trips)")
-
+    fig, ax = Surface3DplotFraction(bikes, policyNames, resultsStarvation, resultsCongestion, "Oslo week 33, Lost trips (" + '%' + ") and starvation/congestion ratio)")
+    
     plt.show()
     print(" bye bye")
