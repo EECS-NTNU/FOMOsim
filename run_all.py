9306e54dd283ec88dbb345b02f4eb379b7962877
Asbjørn Djupdal <djupdal@ntnu.no>
2022-09-22 08:42:30 +0200

    Minor

--- run_all.py
+++ run_all.py
@@ -2,7 +2,7 @@
 
 import os
 
-nodes = os.popen("gstat -a1l | grep compute | awk '{ if(strtonum($8) < 2) printf("%04.1f %s\n", $8, $1); }' | sort | awk '{ print $2 }'").read()
+nodes = os.popen("gstat -a1l").read()
 
 print(nodes)
 
