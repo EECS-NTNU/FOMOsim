6359a4d3397149f2186b98e9c86740cda008e072
Asbjørn Djupdal <djupdal@ntnu.no>
2022-09-28 10:53:10 +0200

    Can now configure some aspects of the run_all script.

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-
 # Distribute all json jobs to the cluster nodes
 
-NODES=(`gstat -i 10.1.1.1 -a1l | grep compute-[13456789] | awk '{ if(strtonum($7) < 2) printf("%s\n", $1); }' | sort -r`)
+# A node times out after this amount of time:
+TIMEOUT="24h"
+
+# These node groups are allowed:
+NODE_GROUPS="13456789"
+
+# Maximum number of nodes this script will use:
+MAX_NODES=1000
+
+# Don't use a node if it has a higher percent load than the following:
+LOAD_LIMIT=2
+
+###############################################################################
+
+NODES=(`gstat -i 10.1.1.1 -a1l | grep compute-[${NODE_GROUPS}] | awk -v limit=${LOAD_LIMIT} '{ if(strtonum($7) < limit) printf("%s\n", $1); }' | sort -r | head -n ${MAX_NODES}`)
+
 RUNS=(`ls experimental_setups`)
 
 num_nodes=${#NODES[@]}
@@ -28,7 +42,7 @@ while [ $run_counter -lt $num_runs ]; do
     node_counter=$((node_counter + 1))
 
     echo "Sending to $node: $args"
-    ssh $node "cd /storage/users/$USER/fomo; python3 run.py $args" > ${node}.out 2> ${node}.err &
+    ssh $node "cd /storage/users/$USER/fomo; timeout ${TIMEOUT} python3 run.py $args" > ${node}.out 2> ${node}.err &
 done
 
 echo "Waiting for completion"
