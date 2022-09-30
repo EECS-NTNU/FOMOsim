c6b3f0d5a2ac6f7670cf3da50d7b689368c5b6b4
Asbjørn Djupdal <djupdal@ntnu.no>
2022-09-30 08:59:00 +0200

    Can now specify nodes to run_all.sh Ctrl-C should now work

--- run_all.sh
+++ run_all.sh
@@ -1,21 +1,55 @@
 #!/bin/bash
 # Distribute all json jobs to the cluster nodes
 
-# A node times out after this amount of time:
-TIMEOUT="24h"
+# General settings
 
-# These node groups are allowed:
-NODE_GROUPS="13456789"
+TIMEOUT="24h"                                # A node times out after this amount of time
+FOMO_DIRECTORY="/storage/users/$USER/fomo"   # Where to find the FOMO directory 
 
-# Maximum number of nodes this script will use:
-MAX_NODES=1000
+# The following settings are only used when finding nodes automatically:
 
-# Don't use a node if it has a higher percent load than the following:
-LOAD_LIMIT=2
+NODE_ROWS="13456789"  # The node rows to pick from
+MAX_NODES=2           # Maximum number of nodes
+LOAD_LIMIT=2          # Don't use a node if it has a higher percent load than this
 
 ###############################################################################
+# Nodes specified on the command line
 
-NODES=(`gstat -i 10.1.1.1 -a1l | grep compute-[${NODE_GROUPS}] | awk -v limit=${LOAD_LIMIT} '{ if(strtonum($7) < limit) printf("%s\n", $1); }' | sort -r | head -n ${MAX_NODES}`)
+if [ $# -gt 0 ] ; then
+
+    while getopts ":r:n:" arg; do
+        case ${arg} in
+            r)
+                ROW=${OPTARG}
+                ;;
+            n)
+                NODENUMS=`echo ${OPTARG} | awk -v RS='[,\n]' -F: 'NF == 1; NF == 2 { for (i = $1; i <= $2; ++i) print i }'`
+                ;;
+            :)
+                echo "$0: Must supply an argument to -$OPTARG." >&2
+                exit 1
+                ;;
+            ?)
+            echo "Invalid option: -${OPTARG}."
+            exit 2
+            ;;
+        esac
+    done
+
+    NODES=""
+
+    for i in $NODENUMS; do
+        NODES="${NODES} compute-${ROW}-$i.local"
+    done
+
+###############################################################################
+# Nothing from command line, find nodes automatically
+
+else
+    NODES=(`gstat -i 10.1.1.1 -a1l | grep compute-[${NODE_ROWS}] | awk -v limit=${LOAD_LIMIT} '{ if(strtonum($7) < limit) printf("%s\n", $1); }' | sort -r | head -n ${MAX_NODES}`)
+fi
+
+###############################################################################
 
 RUNS=(`ls experimental_setups`)
 
@@ -30,20 +64,25 @@ run_counter=0
 rm -rf *.out *.err output.csv
 
 while [ $run_counter -lt $num_runs ]; do
-    args=""
+    node=${NODES[$node_counter]}
+    node_counter=$((node_counter + 1))
 
+    echo "Sending to $node:"
+
+    args=""
     for (( i=0 ; i < $runs_per_node ; i++ )); do
         run=${RUNS[$run_counter]}
+        echo "  " $run
         run_counter=$((run_counter + 1))
         args="$args experimental_setups/$run"
     done
 
-    node=${NODES[$node_counter]}
-    node_counter=$((node_counter + 1))
+    ssh $node "cd ${FOMO_DIRECTORY}; shopt -s huponexit; timeout ${TIMEOUT} python3 run.py $args" > ${node}.out 2> ${node}.err &
 
-    echo "Sending to $node: $args"
-    ssh $node "cd /storage/users/$USER/fomo; timeout ${TIMEOUT} python3 run.py $args" > ${node}.out 2> ${node}.err &
+    echo
 done
+
+trap "kill 0" SIGINT
 
 echo "Waiting for completion"
 wait
