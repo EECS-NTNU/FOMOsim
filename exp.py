117377ba8867f8010f7ed109cccefc48fb13ad4d
Steffen Jaap Bakker <steffen.bakker@ntnu.no>
2022-10-07 12:01:54 +0200

    GH policy change

--- exp.py
+++ exp.py
@@ -41,8 +41,8 @@ instances = [
 analyses = [
     # Name,        target_state,                                 policy,                  numvehicles
     ("do_nothing", target_state.evenly_distributed_target_state, policies.DoNothing(),              1),
-    ("evenly",     target_state.evenly_distributed_target_state, policies.GreedyPolicy(),           1),
-    ("outflow",    target_state.outflow_target_state,            policies.GreedyPolicy(),           1),
+    #("evenly",     target_state.evenly_distributed_target_state, policies.GreedyPolicy(),           1),
+    #("outflow",    target_state.outflow_target_state,            policies.GreedyPolicy(),           1),
     ("equalprob",  target_state.equal_prob_target_state,         policies.GreedyPolicy(),           1),
 ]        
 
