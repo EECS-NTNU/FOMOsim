298aeb98d799bf17f9bc9aa93787872277d9202b
simenaks <100846450+simenaks@users.noreply.github.com>
2022-10-19 18:04:40 +0200

    Worked on mathematical model

--- policies/inngjerdingen_moeller/mathematical_model.py
+++ policies/inngjerdingen_moeller/mathematical_model.py
@@ -16,9 +16,12 @@ def run_model(data):
     W_S = data.W_S
     W_R = data.W_R
 
+    T_bar = data.T_bar
     T_D = data.T_D 
     T_DD = data.T_DD
+    T_W = data.T_W
     T_DW = data.T_DW
+    T_C = data.T_C
     T_DC = data.T_DC
     T_H = data.T_H
     
@@ -27,6 +30,7 @@ def run_model(data):
 
     D = data.D
 
+    Q_0 = data.Q_0
     Q_V = data.Q_V
     Q_S = data.Q_S
 
@@ -35,7 +39,7 @@ def run_model(data):
     #Variables
     c = m.addVar({(i, t) for i in stations for t in time_periods},lb=0, vtype=GRB.CONTINUOUS, name="c")
     s = m.addVar({(i, t) for i in stations for t in time_periods},lb=0, vtype=GRB.CONTINUOUS, name="s")
-    d = m.addVar({(i, t) for i in stations for t in time_periods},lb=0, vtype=GRB.CONTINUOUS, name="d")
+    d = m.addVar({(i) for i in stations},lb=0, vtype=GRB.CONTINUOUS, name="d")
 
     r_B = m.addVar({(i, j, t) for i in stations for j in neighboring_stations[i] for t in time_periods},lb=0, vtype=GRB.CONTINUOUS, name="r_B")
     r_L = m.addVar({(i, j, t) for i in stations for j in neighboring_stations[i] for t in time_periods},lb=0, vtype=GRB.CONTINUOUS, name="r_L")
@@ -50,11 +54,28 @@ def run_model(data):
 
 
     #Constraints
+    #Station balance:
+    #m.addConstr(L_0[i]+quicksum(r_L))
+
+    #Deviations:
+    m.addConstr(d[(i)] >= L_T[i]-l[(i, T_bar)] for i in stations)
+    m.addConstr(d[(i)] >= l[(i,T_bar)]-L_T[i] for i in stations)
+
+    #Vehicle constraints
+    m.addConstr(quicksum(x[(j, i, t-T_DD[i][j], v)] for j in stations_with_source_sink) == quicksum(x[(i, k, t, v)] for k in stations_with_source_sink) for i in stations for t in range(1,T_bar) for v in vehicles)
+    m.addConstr(quickSum(x[(0, j, 0, v)] for j in stations_with_source_sink) == 1 for v in vehicles)
+    m.addConstr(quicksum(x[(i, 0, T_bar-T_DD[i][0], v)] for i in stations_with_source_sink) == 1 for v in vehicles)
+
+    m.addConstr(quicksum(q[(0, i, 0, v)] for i in stations) == Q_0[v] for v in vehicles)
+    m.addConstr(quicksum(q[(j, i, t-T_DD[i][j], v)] for j in stations_with_source_sink) - q_U[(i, v, t)] + q_L[(i, v, t)] == quicksum(q[(i, k, t, v)] for k in stations_with_source_sink) for i in stations for t in range(1, T_bar+1) for v in vehicles)
+    m.addConstr(q[(i, j, t, v)] <= Q_V[v]*x[(i, j, t, v)] for  i in stations for j in stations for t in time_periods for v in vehicles)
 
     
 
-
     #Objective function
 
+    m.setObjective(quicksum(quicksum(W_C*c[(i, t)] + W_S*s[(i, t)] + quicksum(W_R*(T_W[i][j]*r_B[(i, j, t)]+T_C[i][j]*r_L[(i, j, t)]) for j in neighboring_stations[i]) for t in time_periods)+ W_D*d[(i)] for i in stations), GRB.MINIMIZE)
+
+    m.optimize()
     
     return m
