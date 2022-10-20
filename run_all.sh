1b423143f4b00e4242af42380292b6ccd3c5bc05
Steffen Jaap Bakker <steffen.bakker@ntnu.no>
2022-10-20 07:46:33 +0200

    remaining experiments

--- run_all.sh
+++ run_all.sh
@@ -9,7 +9,7 @@ FOMO_DIRECTORY="/storage/users/$USER/fomo"   # Where to find the FOMO directory
 
 # The following settings are only used when finding nodes automatically:
 
-NODE_ROWS="134"  # The node rows to pick from     "13456789"
+NODE_ROWS="4"  # The node rows to pick from     "13456789"
 MAX_NODES=50        # Maximum number of nodes
 LOAD_LIMIT=0.008       # Don't use a node if it has a higher percent load than this
 
