6a00b2c130d0f6ad007272dbf929aeaea1f2373b
Christian Inngjerdingen <chinngje@stud.ntnu.no>
2022-10-28 11:28:58 +0200

    Updated parameters to dict

--- policies/inngjerdingen_moeller/parameters_MILP.py
+++ policies/inngjerdingen_moeller/parameters_MILP.py
@@ -21,7 +21,7 @@ sys.path.insert(0, '') #make sure the modules are found in the new working direc
 
 import sim
 import policies
-from settings import DEFAULT_DEPOT_CAPACITY
+from settings import DEFAULT_DEPOT_CAPACITY, MINUTES_PER_ACTION
 
 # ------------ TESTING DATA MANUALLY ---------------
 source = sim.Station(0,capacity=DEFAULT_DEPOT_CAPACITY)
@@ -32,77 +32,61 @@ station3 = sim.Station(3)
 vehicle1 = sim.Vehicle(1, source, policies.GreedyPolicy(), 0, 6)
 
 class MILP_data():
-    def __init__(self):
-        #Sets
-        # self.stations = [1, 2, 3]
-        self.stations = [station1, station2, station3]
-        # self.stations_with_source_sink = [0, 1, 2, 3]
-        self.stations_with_source_sink = [source, station1, station2, station3]
-        self.neighboring_stations = [[] for i in range(len(self.stations_with_source_sink))]
-        self.vehicles = [vehicle1]
-        self.time_periods = [0,1,2,3,4,5]
+        def __init__(self):
+                #Sets
+                # self.stations = [1, 2, 3]
+                self.stations = [station1, station2, station3]
+                # self.stations_with_source_sink = [0, 1, 2, 3]
+                self.stations_with_source_sink = [source, station1, station2, station3]
+                self.neighboring_stations = dict()      #{station: [list of stations]}
+                self.vehicles = [vehicle1]
+                self.time_periods = [0,1,2,3,4,5]
+                self.possible_previous_stations_driving = dict()        #{(station,time): [list of stations]}
+                self.possible_previous_stations_cycling = dict()        #{(station,time): [list of stations]}
 
+                #Parameters
+                self.W_D = 0.1
+                self.W_C = 0.3
+                self.W_S = 0.3
+                self.W_R = 0.3
 
-        self.possible_previous_stations = [[[],[],[],[],[],[]],[[],[],[],[],[],[]], [[],[],[],[],[],[]], [[],[],[],[],[],[]]] #[station][time_period]
+                self.T_D = dict()       #{(station,station):time}
+                self.T_DD = dict()       #{(station,station):timeperiods}
+                self.T_W = dict()       #{(station,station):time}
+                self.T_DW = dict()       #{(station,station):timeperiods}
+                self.T_C = dict()       #{(station,station):time}
+                self.T_DC = dict()      #{(station,station):timeperiods}
+                
+                self.T_H = MINUTES_PER_ACTION
 
-       
-            #Parameters
-        self.W_D = 0.1
-        self.W_C = 0.3
-        self.W_S = 0.3
-        self.W_R = 0.3
+                self.L_0 = dict()    #{station:inventory}   
+                self.L_T = dict()       #{station:target inventory}
 
-        self.T_D = [[0,0,0,0],
-            [0,0,1,6.7],
-            [0,1,0,3.1], 
-            [0,6.7,3.1,0]]
+                self.D = dict()         #{(station,timeperiod):demand}
 
-        self.T_DD = [[1,0,0,0], #number of timeperiods
-                [0,1,1,2], 
-                [0,1,1,2], 
-                [0,2,1,1]]
+                self.Q_0 = dict()       #{vehicle: inventory}
+                self.Q_V = dict()       #{vehicle: capacity}
+                self.Q_S = dict()       #{station:capacity}
 
-        self.T_W = [[0,0,0,0],
-                [0,0,2,14],
-                [0,2,0,8], 
-                [0,14,8,0]]
+                self.tau = 5    #length of time period
 
-        self.T_DW = [[0,0,0,0],
-                [0,0,1,3],
-                [0,1,0,2], 
-                [0,3,2,0]]
+        # ------------ DEFINING MEMBER FUNCTIONS ---------------
 
-        self.T_C = [[0,0,0,0],
-                [0,0,1,10],
-                [0,1,0,6], 
-                [0,10,6,0]]
+        def set_possible_previous_stations_driving(self):
+                for j in self.stations:
+                        for t in self.time_periods:
+                                for i in self.stations_with_source_sink:
+                                        if(t-self.T_DD[i][j]>=0):
+                                                self.possible_previous_stations_driving[(j,t)] = i
 
-        self.T_DC = [[0,0,0,0],
-                [0,0,1,2],
-                [0,1,0,2], 
-                [0,2,2,0]]
-
-        self.T_H = 0.5
-
-        self.L_0 = [0,15,2,1]
-        self.L_T = [10,10,10,10,10]
-
-        self.D = [[0,0,0,0,0,0], [0,0.5,0.6,0.2,-0.2,-0.1], [0,-0.5,0.6,-0.2,0,-0.2], [0,0.1,0.3,0.1,-0.3,-0.1]]
-
-        self.Q_0 = {vehicle1: 3}
-        self.Q_V = {vehicle1: 6}
-        self.Q_S = [0,20,20,20]
-
-        self.tau = 5
-
-        for j in self.stations:
-                for t in self.time_periods:
-                        for i in self.stations_with_source_sink:
-                                if(t-self.T_DD[i][j]>=0):
-                                        self.possible_previous_stations[j][t].append(i)
-
-        #self.possible_previous_stations = [[[0], [0, 1, 2], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]], [[0], [0, 1, 2], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]], [[0], [0, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]]]
+        def set_possible_previous_stations_cycling(self):
+                for j in self.stations:
+                        for t in self.time_periods:
+                                for i in self.stations_with_source_sink:
+                                        if(t-self.T_DC[i][j]>=0):
+                                                self.possible_previous_stations_cycling[(j,t)] = i
     
 
 d=MILP_data()
+d.set_possible_previous_stations()
 print(d.possible_previous_stations)    
