00172f30ba3c9d7cd7517cb14fa754c1b3f1b44b
simenaks <100846450+simenaks@users.noreply.github.com>
2022-11-15 12:16:02 +0100

    visualizing routes

--- policies/inngjerdingen_moeller/test_subproblem.py
+++ policies/inngjerdingen_moeller/test_subproblem.py
@@ -7,12 +7,13 @@ import policies
 from init_state.wrapper import read_initial_state
 from target_state import equal_prob_target_state, evenly_distributed_target_state
 from helpers import timeInMinutes
+from visualize_subproblem import Visualizer
 
 
 if __name__ == "__main__":
 # ------------ TESTING DATA MANUALLY ---------------
-        #filename = "instances/EH_W31"
-        filename = "instances/TD_W34"
+        filename = "instances/EH_W31"
+        #filename = "instances/TD_W34"
         #filename = "instances/OS_W31"
 
         #tstate = equal_prob_target_state
@@ -21,11 +22,11 @@ if __name__ == "__main__":
         state1 = read_initial_state(filename, tstate)
         policy = policies.GreedyPolicy()
         
-        state1.set_vehicles([policy])
+        state1.set_vehicles([policy, policy])
         
         simul1 = sim.Simulator(
                 initial_state = state1,
-                start_time = timeInMinutes(hours=7), #hours=7, hours=11 or hours=15
+                start_time = timeInMinutes(hours=15), #hours=7, hours=11 or hours=15
                 duration = timeInMinutes(hours=1),
                 verbose = True,
         )
@@ -33,5 +34,8 @@ if __name__ == "__main__":
         d=MILP_data(simul1, 25, 5)
         d.initalize_parameters()
         print("TESTING COMPLETE")
-        run_model(d, True)
+        m=run_model(d, True)
+        v=Visualizer(m,d)
+        v.visualize_route()
+
 # ----------------------------------------------------
