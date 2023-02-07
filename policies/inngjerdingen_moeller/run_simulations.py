1a93e550bf993aafe9570097d8f3c60246b5c208
Christian Inngjerdingen <chinngje@stud.ntnu.no>
2023-02-07 14:13:19 +0100

    Testing multiprocessing

--- policies/inngjerdingen_moeller/run_simulations.py
+++ policies/inngjerdingen_moeller/run_simulations.py
@@ -1,3 +1,14 @@
+######################################################
+import os 
+import sys
+from pathlib import Path
+ 
+path = Path(__file__).parents[2]        # The path seems to be correct either way, sys.path.insert makes the difference
+os.chdir(path)
+# print(os. getcwd())
+sys.path.insert(0, '') #make sure the modules are found in the new working directory
+################################################################
+
 import init_state
 import target_state
 import policies
@@ -5,13 +16,17 @@ import policies.inngjerdingen_moeller
 import sim
 import demand
 from helpers import timeInMinutes
+import time
+
+import multiprocessing as mp
 
 
-def run_simulation(seed, filename, policy):
+
+def run_simulation(seed, policy):
     #change common parameters for the different simulations here:
     START_TIME = timeInMinutes(hours=7)
-    DURATION = timeInMinutes(hours=4)
-    INSTANCE = 'TD_W34'
+    DURATION = timeInMinutes(hours=3)
+    INSTANCE = 'TD_W34_old'
     WEEK = 34
     ###############################################################
     
@@ -29,8 +44,9 @@ def run_simulation(seed, filename, policy):
         verbose = True,
     )
     simulator.run()
-    policies.inngjerdingen_moeller.manage_results.write_sim_results_to_file(filename, simulator, DURATION, append=True)    
-
+    # policies.inngjerdingen_moeller.manage_results.write_sim_results_to_file(filename, simulator, DURATION, append=True)
+    data = policies.inngjerdingen_moeller.manage_results.write_sim_results_to_list(simulator, DURATION)
+    return data
 
 def test_policies(number_of_seeds, policy_dict):
     for policy in policy_dict:
@@ -49,24 +65,38 @@ def test_timehorizons(number_of_seeds, list_of_timehorizons):
 
 def test_weights(number_of_seeds, weight_set):
     for set in weight_set:
-        filename= "weight_set_"+str(set)
+        filename= "weight_set_"+str(set)+".csv"
         policy = policies.inngjerdingen_moeller.InngjerdingenMoellerPolicy(roaming=True, time_horizon=25, weights=weight_set[set])
         for seed in range (1, number_of_seeds+1):
             run_simulation(seed, filename, policy)
+        
         policies.inngjerdingen_moeller.visualize_aggregated_results_2(filename)
 
 def test1(number_of_seeds):
-    filename = "greedy_test.csv"
-    policy = policies.GreedyPolicy()
-    for seed in range (1, number_of_seeds+1):
-            run_simulation(seed, filename, policy)
-    policies.inngjerdingen_moeller.visualize_aggregated_results_2(filename)
+    filename = "multi.csv"
+    policy = policies.inngjerdingen_moeller.InngjerdingenMoellerPolicy(roaming=True,time_horizon=15)
+    pool = mp.Pool(mp.cpu_count())
+    seeds = [i for i in range(number_of_seeds)]
+    data_lists = [pool.apply(run_simulation, args=(seed, policy)) for seed in seeds]
+    pool.close()
+    print(data_lists)
+
+    #------------NON MULTIPROCESSING----------------
+    # result_list = []
+    # for i in range(number_of_seeds):
+    #     result = run_simulation(i, policy)
+    #     result_list.append(result)
+    # print(result_list)
+    # policies.inngjerdingen_moeller.visualize_aggregated_results_2(filename)
 
 
 
 
-# if __name__ == "__main__":
-#     policy_dict = dict(random = policies.RandomActionPolicy(), greedy = policies.GreedyPolicy(), inngjerdingen_moeller = policies.inngjerdingen_moeller.inngjerdingen_moeller.InngjerdingenMoellerPolicy(roaming=True,time_horizon=25))
-#     list_of_timehorizons = [15, 20, 25, 30]
-#     weight_dict = dict(a = [0.3, 0.3, 0.3], b=[0, 0, 0], c=[0, 0, 0], d=[0, 0, 0]) #[W_S, W_R, W_D]
-#     test1(5)
+if __name__ == "__main__":
+    print("Before time starts")
+    start_time = time.time()
+    # policy_dict = dict(random = policies.RandomActionPolicy(), greedy = policies.GreedyPolicy(), inngjerdingen_moeller = policies.inngjerdingen_moeller.InngjerdingenMoellerPolicy(roaming=True,time_horizon=25))
+    # list_of_timehorizons = [15, 20, 25, 30]
+    # weight_dict = dict(a = [0.3, 0.3, 0.3], b=[0, 0, 0], c=[0, 0, 0], d=[0, 0, 0]) #[W_S, W_R, W_D]
+    test1(5)
+    print("Duration with multi: ", time.time()-start_time)
