f4f494b59fff868835e91056c6d57291ecf925bc
simenaks <100846450+simenaks@users.noreply.github.com>
2023-02-14 15:47:13 +0100

    started working on new policy

---
+++ policies/inngjerdingen_moeller/solution_method.py
@@ -0,0 +1,42 @@
+'''rename this module and class eg., PILOT, InngjerdingenMoellerPolicy_2, heuristic or something similar '''
+from policies import Policy
+import sim
+from criticality_score_neighbor import calculate_criticality
+
+class SolutionMethod(Policy):
+    def __init__(self):
+        super().__init__()
+
+    def get_best_action(self, simul, vehicle):
+        next_station = None
+        bikes_to_pickup = []
+        bikes_to_deliver = []  
+        
+        #########################################
+        #               WHAT TO DO              #
+        #########################################
+
+
+        ##########################################
+        #               WHERE TO GO              #
+        ##########################################
+
+        
+        
+        return sim.Action(
+            [],               # batteries to swap
+            bikes_to_pickup, #list of bike id's
+            bikes_to_deliver, #list of bike id's
+            next_station, #id
+        )   
+
+
+    def PILOT_function():
+        #calls the greedy function recursively + smart filtering, branching and depth
+        return None
+    
+    def greedy_with_neighbor():
+        #calls calculate_criticality 
+        #should calculate_criticality recieve potential stations or receive
+        # all stations and make the filtering inside
+        return None
