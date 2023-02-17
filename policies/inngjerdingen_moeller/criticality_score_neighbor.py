5bffa79002c42a7cc30b7d0bb4a9b156dc9c12e0
Christian Inngjerdingen <chinngje@stud.ntnu.no>
2023-02-17 12:11:50 +0100

    Neigh_crit fixes

--- policies/inngjerdingen_moeller/criticality_score_neighbor.py
+++ policies/inngjerdingen_moeller/criticality_score_neighbor.py
@@ -1,12 +1,12 @@
 from policies.gleditsch_hagen.utils import calculate_time_to_violation
 from settings import MAX_ROAMING_DISTANCE, BIKE_SPEED
 
-def calculate_criticality(weights, simul, current_station, potential_stations):
+def calculate_criticality(weights, simul, potential_stations):
     # COMMON DATA
     [w_t, w_dev, w_n, w_dem] = weights
     TIME_HORIZON = 60
-    criticalities = dict()      #{station:[criticalities]}
-    criticalities_summed = dict()
+    criticalities = dict()      #{station:[time_to_viol, dev_t_state, neigh_crit, dem_crit]}
+    criticalities_summed = dict()   #{station:crit_sum}
     time_to_violation_list = []
     deviation_list = []
     neighborhood_crit_list = []
@@ -19,10 +19,10 @@ def calculate_criticality(weights, simul, current_station, potential_stations):
         t_state = potential_station.get_target_state(simul.day(), simul.hour())
         station_type, exp_num_bikes = calculate_station_type(potential_station, net_demand, t_state)
         
-        time_to_violation = calculate_time_to_violation(net_demand, potential_station) #check if this works (hours)
+        time_to_violation = calculate_time_to_violation(net_demand, potential_station) #check if this works (hours) and check numbers returned
         time_to_violation_list.append(time_to_violation)
 
-        deviation_from_t_state = abs(t_state - potential_station.number_of_bikes())
+        deviation_from_t_state = abs(t_state - potential_station.number_of_bikes() - net_demand) # alt. include exp. demand during TH
         deviation_list.append(deviation_from_t_state)
 
         neighborhood_crit = calculate_neighborhood_criticality(simul, potential_station, TIME_HORIZON, station_type)
@@ -42,7 +42,8 @@ def calculate_criticality(weights, simul, current_station, potential_stations):
         criticalities_normalized[station][2] *= w_n
         criticalities_normalized[station][3] *= w_dem
         # Summing all criticalities
-        criticalities_summed[station] = criticalities_normalized[station][0] + criticalities_normalized[station][1] +criticalities_normalized[station][2] + criticalities_normalized[station][3]
+        criticalities_summed[station] = criticalities_normalized[station][0] + criticalities_normalized[station][1] 
+        + criticalities_normalized[station][2] + criticalities_normalized[station][3]
     
 
     #sort the dict?
@@ -52,34 +53,41 @@ def calculate_criticality(weights, simul, current_station, potential_stations):
 def calculate_neighborhood_criticality(simul, potential_station, TIME_HORIZON, station_type):
     neighborhood_crit = 0
     neighbors = potential_station.neighboring_stations  #list
+
     for neighbor in neighbors:
         station_crit = 0
         neighbor_demand = (TIME_HORIZON/60)*(potential_station.get_arrive_intensity(simul.day(), simul.hour()) - potential_station.get_leave_intensity(simul.day(), simul.hour()))
         neighbor_t_state = neighbor.get_target_state(simul.day(), simul.hour())
+        
         # Similarly imbalanced (+)
         neighbor_type, exp_num_bikes = calculate_station_type(neighbor, neighbor_demand, neighbor_t_state)
         if neighbor_type == station_type:
             station_crit += 1
+        
         # Absorb demand (-)
         if station_type == 'p' and neighbor.capacity - exp_num_bikes > 0:
             station_crit -= 1
         elif station_type == 'd' and exp_num_bikes > 0:
             station_crit -= 1
+        
         # Neighbor demand (higher+)
-        station_crit += calculate_demand_criticality(neighbor_type, neighbor_demand)
+        if station_type == neighbor_type:
+            station_crit += calculate_demand_criticality(neighbor_type, neighbor_demand)    #OBS: not normalized
+        
         # Distance scaling (closer+, further-)
         distance = simul.state.traveltime_matrix[potential_station.id][neighbor.id]*BIKE_SPEED
-        station_crit = station_crit*(1-(distance/MAX_ROAMING_DISTANCE))
+        station_crit *= (1-(distance/MAX_ROAMING_DISTANCE))
 
         neighborhood_crit += station_crit
 
     return neighborhood_crit
 
 def calculate_station_type(potential_station, net_demand, t_state):
+    margin = 0.15    # slack around t_state 15%
     exp_num_bikes = potential_station.number_of_bikes() + net_demand
-    if exp_num_bikes - t_state > 0:
+    if exp_num_bikes - t_state > margin*(potential_station.capacity-t_state):
         station_type = "p"  #pick-up
-    elif exp_num_bikes - t_state < 0:
+    elif exp_num_bikes - t_state < -margin*t_state:
         station_type = "d"  #delivery
     else:
         station_type = "b"  #balanced
@@ -98,6 +106,8 @@ def calculate_demand_criticality(station_type, net_demand):
         demand_crit = net_demand
     elif station_type == "b":
         demand_crit = abs(net_demand)
+    else:
+        demand_crit = abs(net_demand)
     return demand_crit
 
 
@@ -106,7 +116,7 @@ def normalize_results(criticalities, time_to_violation_list, deviation_list, nei
     max_deviation = max(deviation_list)
     max_neighborhood = max(neighborhood_crit_list)
     max_demand = max(demand_crit_list)
-    # CHECK THAT NONE OF THESE ARE = 0
+    assert(max_time > 0 and max_deviation > 0 and max_neighborhood > 0 and max_demand > 0, "Some max criticality is not > 0")
     criticalities_normalized = dict()
     for station in criticalities:
         criticalities_normalized[station] = []
