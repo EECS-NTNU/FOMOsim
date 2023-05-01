9c62f195dba54c16088921b06583d164949ef068
simenaks <100846450+simenaks@users.noreply.github.com>
2023-05-01 13:19:40 +0200

    crit weights trondheim

--- policies/inngjerdingen_moeller/run_simulations.py
+++ policies/inngjerdingen_moeller/run_simulations.py
@@ -27,15 +27,15 @@ def run_simulation(seed, policy, duration=24*5, queue=None):
     START_TIME = timeInMinutes(hours=7)
     DURATION = timeInMinutes(hours=duration)
     
-    # INSTANCE = 'TD_W34_old'
-    INSTANCE = 'OS_W31' 
+    INSTANCE = 'TD_W34_old'
+    # INSTANCE = 'OS_W31' 
     # INSTANCE = 'BG_W35'
     # INSTANCE = "NY_W31"
     ###############################################################
     
     state = init_state.read_initial_state("instances/"+INSTANCE)
     state.set_seed(seed)
-    state.set_vehicles([policy, policy]) # this creates one vehicle for each policy in the list
+    state.set_vehicles([policy]) # this creates one vehicle for each policy in the list
     tstate = target_state.USTargetState()
     dmand = demand.Demand()
     simulator = sim.Simulator(
@@ -65,7 +65,7 @@ def test_timehorizons(list_of_seeds, list_of_timehorizons):
 
 def test_criticality_weights(list_of_seeds, criticality_weights_dict):
     for set in criticality_weights_dict:
-        filename= "crit_set_OS_"+str(set)+".csv"
+        filename= "crit_set_TD_"+str(set)+".csv"
         policy=policies.inngjerdingen_moeller.PILOT(criticality_weights_sets=criticality_weights_dict[set])
         test_seeds_mp(list_of_seeds, policy, filename)
 
