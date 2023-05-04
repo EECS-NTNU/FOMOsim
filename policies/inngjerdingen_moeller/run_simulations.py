1c68e78e2a84f02e6590c9ca1f1e48738fd2fa67
simenaks <100846450+simenaks@users.noreply.github.com>
2023-05-04 15:49:23 +0200

    .

--- policies/inngjerdingen_moeller/run_simulations.py
+++ policies/inngjerdingen_moeller/run_simulations.py
@@ -27,8 +27,8 @@ def run_simulation(seed, policy, duration=24*5, queue=None):
     START_TIME = timeInMinutes(hours=7)
     DURATION = timeInMinutes(hours=duration)
     
-    INSTANCE = 'TD_W34_old'
-    # INSTANCE = 'OS_W31' 
+    # INSTANCE = 'TD_W34_old'
+    INSTANCE = 'OS_W31' 
     # INSTANCE = 'BG_W35'
     # INSTANCE = "NY_W31"
     ###############################################################
@@ -77,7 +77,7 @@ def test_evaluation_weights(list_of_seeds, evaluation_weights_dict):
 
 def test_discounting_factors(list_of_seeds, list_of_factors):
      for factor in list_of_factors:
-        filename= "discounting_TD_"+str(factor)+".csv"
+        filename= "discounting_"+str(factor)+".csv"
         policy=policies.inngjerdingen_moeller.PILOT(discounting_factor=factor)
         test_seeds_mp(list_of_seeds, policy, filename)
 
@@ -117,12 +117,13 @@ if __name__ == "__main__":
     # policy_dict = dict(greedy = policies.GreedyPolicy(), greedy_neigh = policies.inngjerdingen_moeller.GreedyPolicyNeighborhoodInteraction())
     # policy_dict = dict(pilot = policies.inngjerdingen_moeller.PILOT())
     # policy_dict = dict(Kloimüllner = policies.inngjerdingen_moeller.PILOT(0, 250))
-    # policy_dict = dict(greedy = policies.GreedyPolicy(), nothing=policies.do_nothing_policy.DoNothing())
+    policy_dict = dict(nothing = policies.do_nothing_policy.DoNothing())
+    # policy_dict = dict(greedy = policies.GreedyPolicy())
     
     # list_of_timehorizons = [20, 30, 40, 50, 60]
     # evaluation_weights = dict(a = [0.4, 0.3, 0.3], b=[0.8, 0.1, 0.1], c=[0.1, 0.8, 0.1], d=[0.1, 0.1, 0.8], e=[0.6, 0.1, 0.3], f=[0.3, 0.6, 0.1], g=[0.3, 0.1, 0.6], h=[0.6, 0.3, 0.1], i=[1.0, 0.0, 0.0], j=[0.45, 0.45, 0.1], k=[0.45, 0.1, 0.45], l=[0.33, 0.33, 0.33], m=[0.9, 0.05, 0.05], n=[0.95, 0.04, 0.01], o=[0.85, 0.1, 0.05], p=[0.9, 0.09, 0.01])
     # criticality_weights = dict(a=[[0.2, 0.2, 0.2, 0.2, 0.2]], b=[[0.3, 0.15, 0.25, 0.2, 0.1]], c=[[0.2, 0.4, 0.2, 0.1, 0.1]], d=[[0.3, 0.3, 0.1, 0.1, 0.2]], e=[[0.2, 0.7, 0.05, 0.05, 0]], f=[[0.05, 0.9, 0.05, 0, 0]], g=[[0.1, 0.6, 0.1, 0.1, 0.1]], h=[[0.3, 0.5, 0, 0, 0.2]], i=[[0.9, 0, 0, 0.1, 0]], j=[[0.7, 0.05, 0.1, 0.1, 0.05]], k=[[0.6, 0.1, 0.05, 0.2, 0.05]], l=[[0.5, 0.05, 0.2, 0.05, 0.2]], m=[[1, 0, 0, 0, 0]], n=[[0, 1, 0, 0, 0]], o=[[0, 0, 1, 0, 0]], p=[[0, 0, 0, 1, 0]], q=[[1, 0, 0, 0, 0]])
-    list_of_factors = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
+    # list_of_factors = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
 
     list_of_seeds=[0,1,2,3,4,5,6,7,8,9]
     # list_of_seeds=[0,1,2,3,4]
@@ -135,8 +136,8 @@ if __name__ == "__main__":
     start_time = time.time()
     # test_evaluation_weights(list_of_seeds=list_of_seeds, evaluation_weights_dict=evaluation_weights)
     # test_criticality_weights(list_of_seeds=list_of_seeds, criticality_weights_dict=criticality_weights)
-    # test_policies(list_of_seeds=list_of_seeds, policy_dict=policy_dict)
-    test_discounting_factors(list_of_seeds, list_of_factors)
+    test_policies(list_of_seeds=list_of_seeds, policy_dict=policy_dict)
+    # test_discounting_factors(list_of_seeds, list_of_factors)
     duration = time.time() - start_time
     print("Running time: ", str(duration))
 
