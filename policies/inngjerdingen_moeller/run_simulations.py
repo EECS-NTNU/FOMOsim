9fcbe0f82119dc512dc5061026a4fccd577e12e4
Christian Inngjerdingen <chinngje@stud.ntnu.no>
2023-05-19 17:06:08 +0200

    TD short

--- policies/inngjerdingen_moeller/run_simulations.py
+++ policies/inngjerdingen_moeller/run_simulations.py
@@ -28,8 +28,8 @@ def run_simulation(seed, policy, duration=24*5, num_vehicles=2, queue=None):
     START_TIME = timeInMinutes(hours=7)
     DURATION = timeInMinutes(hours=duration)
     
-    # INSTANCE = 'TD_W34_old'
-    INSTANCE = 'OS_W31' 
+    INSTANCE = 'TD_W34_old'
+    # INSTANCE = 'OS_W31' 
     # INSTANCE = 'BG_W35'
     # INSTANCE = "NY_W31"
     ###############################################################
@@ -56,7 +56,7 @@ def run_simulation(seed, policy, duration=24*5, num_vehicles=2, queue=None):
 
 def test_policies(list_of_seeds, policy_dict):
     for policy in policy_dict:
-        filename="short_rebal_"+str(policy)+".csv"
+        filename="short_rebal_TD_"+str(policy)+".csv"
         test_seeds_mp(list_of_seeds, policy_dict[policy], filename)
 
 def test_timehorizons(list_of_seeds, list_of_timehorizons):
@@ -101,7 +101,7 @@ def test_num_vehicles(list_of_seeds, vehicles_list):
         policy=policies.inngjerdingen_moeller.PILOT(max_depth=3, number_of_successors=10)
         test_seeds_mp(list_of_seeds, policy, filename, num_vehicles=v)
 
-def test_seeds_mp(list_of_seeds, policy, filename, num_vehicles=2, duration=24*5): #change duration and number of vehicles HERE!
+def test_seeds_mp(list_of_seeds, policy, filename, num_vehicles=1, duration=24*5): #change duration and number of vehicles HERE!
     #------------PROCESS----------------
     seeds = list_of_seeds
     q = mp.Queue()
