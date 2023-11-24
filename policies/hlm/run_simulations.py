8ad811c7f52293e198220a77ded349637d4da4cd
Isabel Lam <itlam@stud.ntnu.no>
2023-11-24 15:53:32 +0100

    ran one test with battery criticality 0.2, 0.15, 0.1

--- policies/hlm/run_simulations.py
+++ policies/hlm/run_simulations.py
@@ -176,7 +176,7 @@ if __name__ == "__main__":
         max_depth = 2, 
         number_of_successors = 5, 
         time_horizon = 40, 
-        criticality_weights_sets = [[0.25, 0.125, 0.225, 0.15, 0.05, 0.0], [0.25, 0.4, 0, 0, 0.2, 0.0], [0.55, 0.08, 0.07, 0.15, 0.05, 0.0]], 
+        criticality_weights_sets = [[0.25, 0.125, 0.225, 0.15, 0.05, 0.2], [0.25, 0.4, 0, 0, 0.2, 0.15], [0.55, 0.08, 0.07, 0.15, 0.05, 0.1]], 
         evaluation_weights = [0.85, 0.1, 0.05], 
         number_of_scenarios = 100, 
         discounting_factor = 0.1
