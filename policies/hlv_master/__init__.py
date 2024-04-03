04fcaa68bb468103f666fdc687313182b9dff5ac
Mathea Berg Vindsetmo <mathea@vindsetmo.no>
2024-04-03 11:07:11 +0200

    collab 4

--- policies/hlv_master/__init__.py
+++ policies/hlv_master/__init__.py
@@ -3,3 +3,4 @@ from .FF_BS_PILOT_policy import BS_PILOT_FF
 from .collab3_X_PILOT_BS import Collab3
 from .collab2_SB_X_PILOT_BS import SB_Collab2
 from .collab2_FF_X_PILOT_BS import FF_Collab2
+from .collab4_X_PILOT_BS import Collab4
