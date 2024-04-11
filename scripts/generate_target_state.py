c53667ba381aa9aaa13233bd14f9606558c63c77
elinehareide <elinehareide@dhcp-10-24-25-34.wlan.ntnu.no>
2024-04-11 08:49:38 +0200

    target

--- scripts/generate_target_state.py
+++ scripts/generate_target_state.py
@@ -8,21 +8,20 @@ import gzip
 
 def generate_json_target_state():
 
-    # with open(FINISHED_DATA_FILE, 'r', encoding='utf-8') as f:
-    #     hex_data = json.load(f)
+     
 
-    hex_data ={
-        'areas': [
+    #hex_data ={
+        #'areas': [
             #  {
             #     "hex_id": "1",
             #     "arrival_intensities": [[0,0,0,0,0, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1] for _ in range(7)],
             #     "depature_intensities": [[0 for i in range(24)] for _ in range(7)],
             #  },
-             {
-                "hex_id": "2",
-                "arrival_intensities": [[0 for _ in range(24)] for _ in range(7)],
-                "depature_intensities": [[0,0,0,0,0, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1] for _ in range(7)],
-             },
+            # {
+            #    "hex_id": "2",
+            #    "arrival_intensities": [[0 for _ in range(24)] for _ in range(7)],
+            #    "depature_intensities": [[0,0,0,0,0, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1] for _ in range(7)],
+            # },
             #  {
             #     "hex_id": "3",
             #     "arrival_intensities": [[0,0,0,0,0, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1] for _ in range(7)],
@@ -38,12 +37,28 @@ def generate_json_target_state():
             #     "arrival_intensities": [[0,0,0,0,0, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1] for _ in range(7)],
             #     "depature_intensities": [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,12] for _ in range(7)],
             #  },
-        ]
-    }
+        #]
+   # }
+    
+    FINISHED_DATA_FILE = "/Users/elinehareide/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Eline - Høst 2023/Prosjektoppgave/fomo/instances/Ryde/TD_W19_test_W3.json.gz"
+    INIT_TARGET_STATE = "/Users/elinehareide/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Eline - Høst 2023/Prosjektoppgave/fomo/instances/Ryde/initial_target_state_1066.json"
+
+    with gzip.open(FINISHED_DATA_FILE, 'rt', encoding='utf-8') as f:
+        hex_data = json.load(f)
+    
+    with open(INIT_TARGET_STATE, 'r', encoding='utf-8') as f:
+         initial_target_state_data = json.load(f)
+
     hexagons = HexWeb.generate_hex_web_target_state_from_json(hex_data)
+   #HexWeb.set_initial_target_state(hexagons, initial_target_state_data)
+    #def set_initial_target_state(hexagons, initial_target_state_data):
+    for init_hex in initial_target_state_data['areas']:
+        for hexagon in hexagons:
+            if hexagon.hex_id == init_hex['id']:
+                hexagon.initial_target_states = init_hex['final_target_states']
+                break
 
     for hex in hexagons:
-        hex.initial_target_states = 100 #finne en måte å gjøre dette på 
         hex.calc_target_state()
     #hexweb.calc_move_probabilities()
 
@@ -59,7 +74,12 @@ def generate_json_target_state():
     # with gzip.open(write_file, 'wt', encoding="ascii") as zipfile:
     #        json.dump(json_data, zipfile)
 
-    with open(FINISHED_TARGET_STATE_DATA_FILE, 'w', encoding='utf-8') as file:
-            json.dump(json_target_state_data, file, ensure_ascii=False, indent=4)
+    FINISHED_TARGET_STATE_DATA_FILE = "/Users/elinehareide/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Eline - Høst 2023/Prosjektoppgave/fomo/instances/Ryde/finished_target_state_1066.json.gz"
+    # Lagre data som en gzip-komprimert JSON-fil
+    with gzip.open(FINISHED_TARGET_STATE_DATA_FILE, 'wt', encoding='ascii') as zipfile:
+        json.dump(json_target_state_data, zipfile)
+    
+    with open("/Users/elinehareide/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Eline - Høst 2023/Prosjektoppgave/fomo/instances/Ryde/finished_target_state_1066_json.json", 'w', encoding='utf-8') as f:
+        json.dump(json_target_state_data, f, indent=4)
 
 generate_json_target_state()
